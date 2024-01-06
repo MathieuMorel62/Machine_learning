32787267601e93a75abc8a9810e26c7a39dcaf90
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-01-06 12:11:05 -0800

    Implement DeepNeuralNetwork class for binary classification with He initialization

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" Module to create a deep neural network """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Deep neural network performing binary classification """
+    def __init__(self, nx, layers):
+      if type(nx) != int:
+        raise TypeError("nx must be an integer")
+      if nx < 1:
+        raise ValueError("nx must be a positive integer")
+
+      if type(layers) != list or not layers:
+        raise TypeError("layers must be a list of positive integers")
+      if any(type(i) != int or i <= 0 for i in layers):
+        raise TypeError("layers must be a list of positive integers")
+
+      self.__L = len(layers)
+      self.__cache = {}
+      self.__weights = {}
+
+      # Initialization of weights and biases
+      for l in range(1, self.__L + 1):
+        self.__weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
+
+        layer_size = layers[l - 1]
+        if l == 1:
+          he_val = np.sqrt(2 / nx)
+          self.__weights['W' + str(l)] = np.random.randn(layer_size, nx) * he_val
+        else:
+          he_val = np.sqrt(2 / layers[l - 2])
+          self.__weights['W' + str(l)] = np.random.randn(layer_size, layers[l - 2]) * he_val
+
+    @property
+    def L(self):
+      return self.__L
+    
+    @property
+    def cache(self):
+      return self.__cache
+    
+    @property
+    def weights(self):
+      return self.__weights
+    
