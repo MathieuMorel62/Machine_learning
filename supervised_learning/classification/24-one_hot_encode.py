078350478ac1599ab84286e23b0e2c020658ffec
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-01-07 04:35:41 -0800

    Added the one_hot_encode function for the conversion of digital labels to one-hot matrix.

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Function that converts a numeric label vector into a one-hot matrix"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+  """converts a numeric label vector into a one-hot matrix"""
+  if not isinstance(Y, np.ndarray) or not isinstance(classes, int):
+    return None
+
+  m = Y.shape[0]
+  one_hot_matrix = np.zeros((classes, m))
+
+  # Filling the one-hot matrix
+  for idx, label in enumerate(Y):
+    if label >= classes:
+      return None
+    one_hot_matrix[label, idx] = 1
+
+  return one_hot_matrix
